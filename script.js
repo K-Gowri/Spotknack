b0beb048da25eb18f8b72c2183923298d6cf4971
Kabilesh-GS <kabileshgs@gmail.com>
2023-10-07 14:18:59 +0530

    completed JS for the table

--- script.js
+++ script.js
@@ -1,16 +1,22 @@
-const tabs= document.querySelectorAll('.tab_btn');
-const all_content= document.querySelectorAll('.content');
-tabs.forEach((tab, index)=>{
-    tab.addEventListener('click',(e)=>{
-        tabs.forEach(tab=>{tab.classList.remove('active')});
-        tab.classList.add('active');
-        
-        var line = document.querySelector('.line');
-        line.style.width=e.target.offsetWidth+"px";
-        line.style.left=e.target.offsetLeft+"px";
-        
-        all_content.forEach(content=>{content.classList.remove('active')});
-        all_content[index].classList.add('active');
-    })
-   
-})
+let cointabl = document.getElementById("cointable");
+let giftcardtabl = document.getElementById("giftcardtable");
+let swit1 = document.getElementById("switch1");
+let swit2 = document.getElementById("switch2");
+
+function displaytable1() {
+  cointabl.style.display = "block";
+  giftcardtabl.style.display = "none";
+  swit1.style.backgroundColor = "black";
+  swit1.style.color = "white";
+  swit2.style.backgroundColor = "#f5f5f5";
+  swit2.style.color = "black";
+}
+
+function displaytable2() {
+  cointabl.style.display = "none";
+  giftcardtabl.style.display = "block";
+  swit2.style.backgroundColor = "black";
+  swit2.style.color = "white";
+  swit1.style.backgroundColor = "#f5f5f5";
+  swit1.style.color = "black";
+}
